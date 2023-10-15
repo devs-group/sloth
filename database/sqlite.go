2eb0ffdc297525326b7a2d5be72ded3452aa190d
Robert <robert@roberts-mbp.home>
2023-10-15 12:32:38 +0200

    Create db file and folder if it does not exist

--- database/sqlite.go
+++ database/sqlite.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"log/slog"
+	"os"
+	"path/filepath"
 
 	"github.com/jmoiron/sqlx"
 	_ "modernc.org/sqlite"
@@ -15,6 +17,26 @@ type Store struct {
 
 func connect() {
 	var err error
+	path := "./database/database.sqlite"
+
+	if err := os.MkdirAll(filepath.Dir(path), os.ModePerm); err != nil {
+		slog.Error("unable to create directory", "err", err)
+		panic(err)
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if _, err := os.Create(path); err != nil {
+			slog.Error("unable to create file", "err", err)
+			panic(err)
+		}
+		slog.Info("created directory and file", "path", path)
+	} else if err != nil {
+		slog.Error("unable to check file", "err", err)
+		panic(err)
+	}
+
+	slog.Info("connecting to sqlite db...")
+
 	DB, err = sqlx.Open("sqlite", "./database/database.sqlite")
 	if err != nil {
 		slog.Error("unable to connect to sqlite db", "err", err)
