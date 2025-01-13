0c33178d3b2444b463fb9c2af3ee21eaa7efab7e
Ralph Segi <segiralph@gmail.com>
2025-01-13 13:14:30 -0600

    Fix login with multiple providers resolving to the same email

--- backend/services/user.go
+++ backend/services/user.go
@@ -1,7 +1,7 @@
 package services
 
 import (
-	"log/slog"
+	"database/sql"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -29,7 +29,7 @@ type AuthMethod struct {
 }
 
 func (g *User) GetUserWithSocialID(socialID string, tx *sqlx.Tx) (bool, error) {
-	query := "SELECT * FROM auth_methods a LEFT JOIN user u ON a.user_id = u.user_id WHERE a.social_id=$1"
+	query := "SELECT * FROM auth_methods a LEFT JOIN users u ON a.user_id = u.user_id WHERE a.social_id=$1"
 	var user User
 	if err := tx.Get(&user, query, socialID); err != nil {
 		return false, err
@@ -38,6 +38,15 @@ func (g *User) GetUserWithSocialID(socialID string, tx *sqlx.Tx) (bool, error) {
 	*g = user
 
 	return true, nil
+}
+
+func GetUserByMail(email string, tx *sqlx.Tx) (*User, error) {
+	query := "SELECT u.* FROM users u JOIN auth_methods a ON a.user_id = u.user_id WHERE u.email=$1 LIMIT 1"
+	var user User
+	if err := tx.Get(&user, query, email); err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
 func UpsertUserBySocialIDAndMethod(methodType string, user *goth.User, tx *sqlx.Tx) (int, error) {
@@ -49,23 +58,37 @@ func UpsertUserBySocialIDAndMethod(methodType string, user *goth.User, tx *sqlx.
 		return userID, nil
 	}
 
-	var email *string
+	var email string
 	emailIsVerified := false
 	if user.Email != "" {
 		emailIsVerified = true
-		email = &user.Email
+		email = user.Email
 	}
 
-	query = `INSERT INTO users (email, username, email_verified) VALUES( $1, $2, $3 ) RETURNING user_id;`
-	err = tx.Get(&userID, query, email, user.NickName, emailIsVerified)
+	// The user can still exists, just not with the given methodType
+	existingUser, err := GetUserByMail(email, tx)
 	if err != nil {
-		return 0, errors.Wrap(err, "can't insert new user")
+		if !errors.Is(err, sql.ErrNoRows) {
+			return 0, errors.Wrap(err, "can't select existing user")
+		}
 	}
 
+	// Only create the user if we can't find an entry
+	if existingUser == nil {
+		query = `INSERT INTO users (email, username, email_verified) VALUES( $1, $2, $3 ) RETURNING user_id;`
+		err = tx.Get(&userID, query, email, user.NickName, emailIsVerified)
+		if err != nil {
+			return 0, errors.Wrap(err, "can't insert new user")
+		}
+	} else {
+		// Otherwise make sure we set the existing user ID
+		userID = existingUser.UserID
+	}
+
+	// We always insert the auth_method which happens only once per user and method
 	query = `INSERT INTO auth_methods( user_id, method_type, social_id ) VALUES ( $1, $2, $3 );`
 	if _, err = tx.Exec(query, userID, methodType, user.UserID); err != nil {
-		slog.Error("ERROR", "found ERROR", err)
-		return 0, err
+		return 0, errors.Wrap(err, "can't insert auth method")
 	}
 
 	return userID, nil
